36dadee17ba201e7d41f3086a3370e416aee1d71
Vittorio Pippi <219424@studenti.unimore.it>
2023-02-08 18:23:44 +0100

    exclude nodes

--- configs/training/base.sh
+++ configs/training/base.sh
@@ -4,10 +4,12 @@
 #SBATCH -e /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_%j.err
 #SBATCH -o /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_%j.out
 #SBATCH -J bilama
+#SBATCH --exclude=aimagelab-srv-00,aimagelab-srv-10,vegeta,carabbaggio
 
 source activate LaMa
 cd /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama || exit
-srun --comment="bilama_@{n_blocks}_@{operation}_@{att}" python3 train.py -c base --n_blocks @{n_blocks|9} \
+scontrol update JobID="$SLURM_JOB_ID" name="bilama_@{n_blocks}_@{operation}_@{att}"
+srun python3 train.py -c base --n_blocks @{n_blocks|9} \
               --operation "@{operation|ffc}" --attention @{att|none} --num_workers 4 --epochs 500 \
               --train_data_path \
               /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO09 \
