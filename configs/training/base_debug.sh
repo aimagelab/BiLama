72735f223efdd273312eaf3fe5f62016a8c1ce35
Vittorio Pippi <219424@studenti.unimore.it>
2023-02-08 17:07:59 +0100

    Changed conv 3x3 to conv 1x1

--- configs/training/base_debug.sh
+++ configs/training/base_debug.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#SBATCH --gres=gpu:2080:1
-#SBATCH --partition=prod
-#SBATCH -e /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_%j.err
-#SBATCH -o /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_%j.out
-#SBATCH -J bilama
+#SBATCH --gres=gpu:1
+#SBATCH --partition=dev
+#SBATCH -e /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_debug_%j.err
+#SBATCH -o /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_debug_%j.out
+#SBATCH --mem-per-gpu=18G
+#SBATCH --cpus-per-gpu=6
+#SBATCH -J debug
 
 source activate LaMa
 cd /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama || exit
 srun --comment="bilama_@{n_blocks}_@{operation}_@{att}" python3 train.py -c base --n_blocks @{n_blocks|9} \
-              --operation "@{operation|ffc}" --attention @{att|none} --num_workers 4 --epochs 500 \
+              --operation "@{operation|ffc}" --attention @{att|none} --num_workers 1 --epochs 500 \
               --train_data_path \
               /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO09 \
               /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO10 \
