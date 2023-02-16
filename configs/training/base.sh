71cab742ee906024a779c3d0a3146adf94ae9dff
Vittorio Pippi <219424@studenti.unimore.it>
2023-02-16 17:06:12 +0100

    Moved datasets to SSD

--- configs/training/base.sh
+++ configs/training/base.sh
@@ -11,18 +11,17 @@ source activate LaMa
 cd /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama || exit
 scontrol update JobID="$SLURM_JOB_ID" name="bilama_@{n_blocks}_@{operation}_@{att}_SKIP@{skip}_patch"
 srun python3 train.py -c base --n_blocks @{n_blocks|9} \
-              --operation "@{operation|ffc}" --attention @{att|none} --num_workers 2 --epochs 500 --skip @{skip} --unet_layers ${unet} \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO09 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO10 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO11 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO12 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO13 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO14 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO16 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO17 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO19 \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DirtyDocuments \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/PALM \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/patch_square \
+              --operation "@{operation|ffc}" --attention @{att|none} --num_workers 2 --epochs 500 --skip @{skip} --unet_layers @{unet} \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO09 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO10 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO11 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO12 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO13 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO14 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO16 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO17 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO19 \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DirtyDocuments \
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/PALM \
               --test_data_path \
-              /mnt/beegfs/work/FoMo_AIISDH/datasets/DIBCO18
+              /mnt/beegfs/work/FoMo_AIISDH/datasets/datasets_refactoring/DIBCO18
