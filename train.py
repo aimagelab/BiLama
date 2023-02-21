491fa98a2f7221a1123949406f7a89e983d10f8f
Fabio Quattrini <38918787+quattrinifabio@users.noreply.github.com>
2023-02-21 14:38:08 +0100

    Update train.py

--- train.py
+++ train.py
@@ -195,7 +195,7 @@ def train(config_args, config):
                     psnr_running_mean = sum(trainer.psnr_list[-3:]) / len(trainer.psnr_list[-3:])
                     if valid_metrics['psnr'] > trainer.best_psnr:
                         trainer.best_psnr = valid_metrics['psnr']
-                    if valid_metrics['psnr'] > trainer.best_psnr_running_mean:
+                    if psnr_running_mean > trainer.best_psnr_running_mean:
                         trainer.best_psnr_running_mean = psnr_running_mean
                         patience = config['patience']
                         if 'precision' in valid_metrics and 'recall' in valid_metrics:
