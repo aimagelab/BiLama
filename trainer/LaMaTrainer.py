c943a435fe432102cb0b40d1cb9b5a5bdfca84cb
Vittorio Pippi <219424@studenti.unimore.it>
2023-02-24 12:29:22 +0100

    Fix resume bug

--- trainer/LaMaTrainer.py
+++ trainer/LaMaTrainer.py
@@ -54,6 +54,7 @@ class LaMaTrainingModule:
             self.checkpoint = torch.load(config['resume'])
             checkpoint_config = self.checkpoint['config'] if 'config' in self.checkpoint else {}
             self.config = self.config | checkpoint_config
+            config = self.config
 
         self.training_only_with_patch_square = False
         if len(config['train_data_path']) == 1 and 'patch_square' in config['train_data_path'][0]:
