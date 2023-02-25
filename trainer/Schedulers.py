0330992207f3d81f5fd58dfebb34c473333478ee
Fabio Quattrini <38918787+quattrinifabio@users.noreply.github.com>
2023-02-25 19:33:30 +0100

    Update Schedulers.py

--- trainer/Schedulers.py
+++ trainer/Schedulers.py
@@ -2,6 +2,35 @@ import torch
 from ignite.handlers import create_lr_scheduler_with_warmup
 
 
+class WarmupScheduler:
+    def __init__(self, scheduler, lr_min, warmup):
+        self.scheduler = scheduler
+        self.warmup = warmup
+        self.lr_min = lr_min
+        self.warmup_scheduler = torch.optim.lr_scheduler.LambdaLR(scheduler.optimizer, lambda epoch: min(epoch / warmup, 1))
+        self.current_epoch = 0
+
+    def step(self, metrics=None):
+        if self.current_epoch < self.warmup:
+            self.warmup_scheduler.step()
+        else:
+            self.scheduler.step(metrics)
+        self.current_epoch += 1
+
+    def get_lr(self):
+        if self.current_epoch < self.warmup:
+            scheduler = self.warmup_scheduler
+        else:
+            scheduler = self.scheduler
+        return [params['lr'] for params in scheduler.optimizer.param_groups]
+
+    def state_dict(self):
+        return self.scheduler.state_dict().update({'last_epoch': self.current_epoch})
+
+    def load_state_dict(self, state_dict):
+        self.current_epoch = state_dict['last_epoch']
+        return self.scheduler.load_state_dict(state_dict)
+
 def make_lr_scheduler(kind, optimizer, kwargs, warmup, config):
     lr = config['learning_rate']
     lr_min = config['learning_rate_min']
@@ -13,12 +42,13 @@ def make_lr_scheduler(kind, optimizer, kwargs, warmup, config):
     elif kind == 'multistep':
         lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, **kwargs)
     elif kind == 'step':
-        kwargs = dict(step_size=100, gamma=0.5) | kwargs
+        kwargs = dict(step_size=100, gamma=0.5).update(kwargs)
         lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, **kwargs)
     elif kind == 'cosine':
         lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=lr_min)
     elif kind == 'linear':
-        func = lambda epoch: lr - epoch * (lr - lr_min) / epochs
+        lr_ratio = lr_min / lr
+        func = lambda epoch: epoch / epochs * (lr_ratio - 1) + 1
         lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=func)
     elif kind == 'plateau':
         kwargs_default = dict(mode='max', factor=0.5, patience=config['patience'] // 2)
@@ -28,7 +58,7 @@ def make_lr_scheduler(kind, optimizer, kwargs, warmup, config):
         raise ValueError(f"Unknown kind of lr scheduler: {kind}")
 
     if warmup > 0:
-        lr_scheduler = create_lr_scheduler_with_warmup(lr_scheduler, lr_min, warmup)
+        lr_scheduler = WarmupScheduler(lr_scheduler, lr_min, warmup)
 
     # plot the scheduler
     # import matplotlib.pyplot as plt
@@ -37,6 +67,7 @@ def make_lr_scheduler(kind, optimizer, kwargs, warmup, config):
     # for i in range(epochs):
     #     lr_scheduler.step()
     #     lrs.append(lr_scheduler.get_lr()[0])
+    # print(lrs)
     # plt.plot(np.arange(epochs), lrs)
     # plt.show()
 
