89723b653aa9ba38c74e5246bf5766f3ff7d58b8
Fabio Quattrini <38918787+quattrinifabio@users.noreply.github.com>
2023-03-14 13:18:31 +0100

    Update base_512.sh

--- configs/training/base_512.sh
+++ configs/training/base_512.sh
@@ -4,7 +4,7 @@
 #SBATCH -e /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_512_%j.err
 #SBATCH -o /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama/jobs/bilama_512_%j.out
 #SBATCH --mem=24G
-#SBATCH --exclude=aimagelab-srv-00,aimagelab-srv-10,vegeta,carabbaggio
+#SBATCH --exclude=aimagelab-srv-00,aimagelab-srv-10,vegeta,carabbaggio,germano,gervasoni,pippobaudo,rezzonico,ajeje,helmut,lurcanio
 #SBATCH -J bilama_abla
 
 cd /mnt/beegfs/work/FoMo_AIISDH/vpippi/BiLama || exit
@@ -13,7 +13,7 @@ srun /homes/$(whoami)/.conda/envs/LaMa/bin/python /mnt/beegfs/work/FoMo_AIISDH/v
   --n_blocks @{n_blocks|3} --operation @{operation|ffc} --attention none --num_workers 2 --epochs 500 --skip cat \
   --unet_layers @{unet_layers|2} --lr_scheduler cosine --lr_scheduler_kwargs "dict()" --resume @{resume|none} --ema_rate -1 \
   --loss @{loss|CHAR} --merge_image false --train_transform_variant latin --lr_scheduler_warmup 10 \
-  --patch_size 512 --patch_size_raw 784 --datasets \
+  --patch_size 512 --patch_size_raw 784 --batch_size 4 --datasets \
     /mnt/beegfs/scratch/fquattrini/binarization_datasets_one_for_eval/DIBCO09 \
     /mnt/beegfs/scratch/fquattrini/binarization_datasets_one_for_eval/DIBCO10 \
     /mnt/beegfs/scratch/fquattrini/binarization_datasets_one_for_eval/DIBCO11 \
